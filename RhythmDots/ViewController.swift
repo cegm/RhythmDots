e99017f9fe1a5ecc27a7a6c792d527f14743f168
CARLOS EDUARDO GIL MEZTA <cgilm@102mac10.local>
2018-07-09 11:18:08 -0500

    Initial Commit

---
+++ RhythmDots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RhythmDots
+//
+//  Created by CARLOS EDUARDO GIL MEZTA on 09/07/18.
+//  Copyright © 2018 Eduardo Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
