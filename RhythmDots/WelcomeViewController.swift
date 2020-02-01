912ff129c3f4fae5dfbdae8ef99b6db329306f1d
cegm <eduardo.gil.mezta@gmail.com>
2020-01-31 22:09:41 -0600

    log-in with google works

--- RhythmDots/WelcomeViewController.swift
+++ RhythmDots/WelcomeViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import FirebaseUI
+import FirebaseDatabase
+import FirebaseAuth
 
-class WelcomeViewController: UIViewController {
+class WelcomeViewController: UIViewController, FUIAuthDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +25,16 @@ class WelcomeViewController: UIViewController {
     }
     
     @IBAction func masterButtonPushed(_ sender: UIButton) {
-        
+        if Auth.auth().currentUser != nil {
+            performSegue(withIdentifier: "SettingsViewController", sender: self)
+        } else {
+            let authUI = FUIAuth.defaultAuthUI()
+            authUI?.delegate = self
+            let providers: [FUIAuthProvider] = [FUIGoogleAuth()]
+            authUI?.providers = providers
+            let authViewController = authUI!.authViewController()
+            self.present(authViewController, animated: true, completion: nil)
+        }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
@@ -31,6 +43,23 @@ class WelcomeViewController: UIViewController {
             let vc = segue.destination as? GameViewController
             vc?.master = false
         }
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        // Handle user returning from authenticating
+        if user != nil {
+            performSegue(withIdentifier: "SettingsViewController", sender: self)
+        }
+    }
+    
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        // other URL handling goes here.
+        return false
     }
     
     
